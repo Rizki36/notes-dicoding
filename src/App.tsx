caa68dfe30653a77456585ad0f11fe7f0130dc7f
Rizki36 <cahrizki.rf@gmail.com>
2022-06-01 11:12:31 +0700

    add title to view

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import './App.css';
 
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+export type NotesType = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+class App extends React.Component<any, { notes: NotesType[] }, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      notes: [],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-header__title">Notes App</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
